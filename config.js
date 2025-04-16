1252e803c071e900903230fdcbe0ae694d908b51
Soumya Halder <121659332+localmirror@users.noreply.github.com>
2025-04-16 19:07:09 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const BOT_TOKEN = '<YOUR_BOT_TOKEN>';
-export const ADMIN_IDS = ['123456789']; // Telegram user IDs of the admins
-export const REQUIRED_CHANNELS = ['@yourchannel1', '@yourchannel2'];
-export const MODE = 'manual'; // 'manual' or 'auto'
