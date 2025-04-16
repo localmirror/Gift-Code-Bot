98bdda7300c717ab6eb3cc8361e16ef0a6b3954c
Soumya Halder <121659332+localmirror@users.noreply.github.com>
2025-04-16 19:48:46 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    token: 'YOUR_BOT_TOKEN',
-    adminId: 'YOUR_ADMIN_ID',
-    dbURL: 'YOUR_DATABASE_URL',
-    forceSubscribe: true,
-    allowedChannels: ['channel1', 'channel2'],
-    approvalMode: 'automatic'  // 'automatic' or 'manual'
-};
