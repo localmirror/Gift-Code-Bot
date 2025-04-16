4dcac2477f1f927ef081d298590ea3cb1980a884
Soumya Halder <121659332+localmirror@users.noreply.github.com>
2025-04-16 19:15:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    token: 'YOUR_BOT_TOKEN',
+    adminId: 'YOUR_ADMIN_ID',
+    dbURL: 'YOUR_DATABASE_URL',
+    forceSubscribe: true,
+    allowedChannels: ['channel1', 'channel2'],
+    approvalMode: 'automatic'  // 'automatic' or 'manual'
+};
