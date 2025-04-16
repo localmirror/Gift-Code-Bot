1b975a37a3bd53962cf68fce6f7464ea08d05ee5
Soumya Halder <121659332+localmirror@users.noreply.github.com>
2025-04-16 18:52:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+export const BOT_TOKEN = '<YOUR_BOT_TOKEN>';
+export const ADMIN_IDS = ['123456789']; // Telegram user IDs of the admins
+export const REQUIRED_CHANNELS = ['@yourchannel1', '@yourchannel2'];
+export const MODE = 'manual'; // 'manual' or 'auto'
